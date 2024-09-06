1266f203ec4c23c5e7848f801ac5e8b1fcacb575
Kyle <notxap55@yahoo.com>
2024-09-06 11:19:45 -0700

    exception handling

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+data = pandas.read_csv('nato_phonetic_alphabet.csv')
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+def generate_phonetic():
+    word = input("What is the word? ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Only letters please!")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
-nato_file = pandas.read_csv('nato_phonetic_alphabet.csv')
-
-new_dict = {row[0]:row[1] for row in nato_file.values}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = list(input("What is the word? ").upper())
-print(user_input)
-
-# loop over letters in input, return a list with corresponding words
-input_nato = [letter for letter in user_input if ]
+generate_phonetic()
